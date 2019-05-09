5a978b4e89c41d70ec383962d14ab56105eecd8f
rdm10 <robertmouncer@gmail.com>
2019-05-09 14:43:22 +0100

    find the best parameters for the RFC

---
+++ Assignment 2.py
@@ -0,0 +1,91 @@
+
+# coding: utf-8
+
+# Read in files
+# fill in blank values
+
+# In[2]:
+
+
+import pandas as pd
+from sklearn.svm import SVR
+
+train = pd.read_csv("./train_signal.csv").fillna(0)
+featcsv = pd.read_csv("./train_feat.csv").fillna(0)
+
+
+# Change the type to integers
+
+# In[3]:
+
+
+import numpy as np
+
+train["Type"]=np.where(train["Type"]=="~",0,
+                      np.where(train["Type"]=="A",1,
+                      np.where(train["Type"]=="N",2,3)))
+
+
+# In[4]:
+
+
+X = featcsv.values[:,2:]
+y = train["Type"].values
+
+
+# In[17]:
+
+
+def report(results, n_top=3):
+    file = open("report.txt","w") 
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
+            
+ 
+            file.write("Model with rank: {0}".format(i)) 
+            file.write("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate])) 
+            file.write("Parameters: {0}".format(results['params'][candidate])) 
+            file.write("\n\r") 
+
+    file.close() 
+
+
+# In[18]:
+
+
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(n_estimators=20)
+
+param_grid = {"n_estimators": range(30,60),
+              "max_depth": range(5,20)}
+
+grid_search = GridSearchCV(clf, param_grid=param_grid, cv=4)
+grid_search.fit(X, y)
+report(grid_search.cv_results_)
+
+
+# pip3 install keras tensorflow tensorflow-gpu numpy pandas sklearn
+
+# In[41]:
+
+
+
+import heapq
+features = grid_search.best_estimator_.feature_importances_.tolist()
+print(features)
+heapq.nlargest(10, xrange(len(features)), key=features.__getitem__)
+
+    
+
