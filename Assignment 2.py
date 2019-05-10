0933b38aaed5c8bbc991d3340df8bc3de367eb18
Robert David Mouncer <rdm10@aber.ac.uk>
2019-05-10 11:05:33 +0100

    weights changed to algorithms

--- Assignment 2.py
+++ Assignment 2.py
@@ -37,7 +37,7 @@ y = train["Type"].values
 # In[17]:
 
 
-def report(results, n_top=3,classifier):
+def report(results,classifier, n_top=3):
     file = open(classifier,"w") 
     for i in range(1, n_top + 1):
         candidates = np.flatnonzero(results['rank_test_score'] == i)
@@ -87,9 +87,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 clf = KNeighborsClassifier()
 
-param_grid = {"n_neighbors": range(1,10) + [20,50,100,200],
+param_grid = {"n_neighbors": list(range(1,10)) + [20,50,100,200],
               "weights": ["ball_tree","kd_tree","brute"],
-              "leaf_size": range(1,10) + [20,50,100]}
+              "leaf_size": list(range(1,10)) + [20,50,100]}
 
 grid_search = GridSearchCV(clf, param_grid=param_grid,n_jobs=10,verbose=2, cv=4)
 grid_search.fit(X, y)
