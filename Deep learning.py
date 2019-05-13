e16e99d7a8d27fc9d34caa0bbe6749531c1e09ea
Robert Mouncer <robertmouncer@gmail.com>
2019-05-13 21:14:54 +0100

    deep learning started and working

--- Deep learning.py
+++ Deep learning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import Flatten, Dense, Conv1D
+import tensorflow as tf
 import numpy as np
 
 print("reading in files...")
@@ -8,16 +9,13 @@ train = pd.read_csv("./train_signal.csv").fillna(0)
 
 test = pd.read_csv("./test_signal.csv").fillna(0)
 print("read in files...")
+train.head()
 
 # In[3]:
-
-
-
 
 train["Type"]=np.where(train["Type"]=="~",0,
                       np.where(train["Type"]=="A",1,
                       np.where(train["Type"]=="N",2,3)))
-
 
 # In[4]:
 X = train.values[:,2:]
@@ -29,28 +27,31 @@ def outputResults(output_data,filename):
     
     df = pd.DataFrame(output_data, columns = ['ID','Predicted'])
     df.to_csv(filename, index=None, header=True)
+        
+# In[3]:
+from keras.utils import to_categorical
+
+y_train = to_categorical(y)
+print(train["Type"])
 # In[3]:
 print("Building model...")
-
-epochs = 3
-batch_size = 50
+input_shape = X[1].shape
 
 model = Sequential()
-
-model.add(Conv2D(2, kernel_size=3, activation=’relu’))
-
-model.add(Conv2D(2, kernel_size=3, activation='relu'))
-
-model.add(Flatten())
-
-
-#output layer
+model.add(Dense(units=20, activation='relu', input_shape=input_shape))
 model.add(Dense(4, activation='softmax'))
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
 
-
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-model.fit(X,y,epochs=epochs,batch_size=batch_size)
-
+# In[3]:
+model.fit(X, y_train, epochs=5)
+# In[3]:
 prediction = model.predict(testX)
-print(prediction)
+predmax = np.argmax(prediction,1)
+
+#predmax = np.where(predmax==0,'~',np.where(predmax==1,'A',np.where(predmax==2,'N','O')))
+res = np.array(list("~ANO"))[predmax]
+
+output_data = {'ID':list(test.values[:,0]),
+                   'Predicted': list(res)}
+
+outputResults(output_data,"knnResults.csv")
