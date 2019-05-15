2651b5665f5bdd15f8cb8dea385ed6ee59efc905
Robert Mouncer <robertmouncer@gmail.com>
2019-05-15 21:12:35 +0100

    rejoined files

--- Deep learning.py
+++ Deep learning.py
@@ -1,6 +1,55 @@
-filters =
+filters = 90
 k_size = 15
 epochs = 8
+
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import  Dense, Conv1D, GlobalAveragePooling1D, MaxPooling1D, Dropout
+import numpy as np
+
+print("reading in files...")
+train = pd.read_csv("./train_signal.csv").fillna(0)
+
+test = pd.read_csv("./test_signal.csv").fillna(0)
+print("read in files...")
+train.head()
+
+
+
+train["Type"]=np.where(train["Type"]=="~",0,
+                      np.where(train["Type"]=="A",1,
+                      np.where(train["Type"]=="N",2,3)))
+
+
+X = train.values[:,2:]
+
+
+testX = test.values[:,1:]
+y = train["Type"].values
+
+print("data preprocessing...")
+#https://thispointer.com/pandas-find-duplicate-rows-in-a-dataframe-based-on-all-or-selected-columns-using-dataframe-duplicated-in-python/
+dfObj = pd.DataFrame(X)
+firstDups = dfObj[dfObj.duplicated()]
+lastDups = dfObj[dfObj.duplicated(keep='last')]
+duplicates = list(firstDups.index.values) + list(lastDups.index.values)
+
+
+#print(list(firstDups.index.values))
+X = np.delete(X,list(duplicates),axis=0)
+y = np.delete(y,list(duplicates),axis=0)
+
+def outputResults(output_data,filename):
+    
+    df = pd.DataFrame(output_data, columns = ['ID','Predicted'])
+    df.to_csv(filename, index=None, header=True)
+
+from keras.utils import to_categorical
+
+y_train = to_categorical(y)
+
+input_shape = (X.shape[1],1)
+
 
 print("begin building model...")
 # In[3]:
@@ -27,7 +76,7 @@ model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['acc
 x_train_reshape = X.reshape(X.shape[0], X.shape[1],1)
 model.fit(x_train_reshape, y_train, epochs=epochs)
 # In[3]:
-testX_reshape = testX.reshape(testX.shape[0],testX[1],1)
+testX_reshape = testX.reshape(testX.shape[0],testX.shape[1],1)
 prediction = model.predict(testX_reshape)
 predmax = np.argmax(prediction,1)
 
