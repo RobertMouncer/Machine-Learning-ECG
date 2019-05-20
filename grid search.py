fc88f5a4d09e02e4284193b8c6a191420191a24e
Robert Mouncer <robertmouncer@gmail.com>
2019-05-20 22:35:36 +0100

    boost added to grid search

--- grid search.py
+++ grid search.py
@@ -5,39 +5,19 @@
 
 
 import pandas as pd
-print("reading in files...")
-train = pd.read_csv("./train_signal.csv").fillna(0)
+
 featcsv = pd.read_csv("./train_feat.csv").fillna(0)
-
-#test = pd.read_csv("./test_signal.csv").fillna(0)
-#testFeatCsv = pd.read_csv("./test_feat.csv").fillna(0)
-print("read in files...")
-
-
-# Change the type to integers
-
-# In[3]:
-
 
 import numpy as np
 
-train["Type"]=np.where(train["Type"]=="~",0,
-                      np.where(train["Type"]=="A",1,
-                      np.where(train["Type"]=="N",2,3)))
-
-#test["Type"]=np.where(test["Type"]=="~",0,
-#                      np.where(test["Type"]=="A",1,
-#                      np.where(test["Type"]=="N",2,3)))
-# In[4]:
+featcsv["Type"]=np.where(featcsv["Type"]=="~",0,
+                      np.where(featcsv["Type"]=="A",1,
+                      np.where(featcsv["Type"]=="N",2,3)))
 
 
 X = featcsv.values[:,2:]
-#testX = testFeatCsv.values[:,2:]
-y = train["Type"].values
 
-
-# In[17]:
-
+y = featcsv["Type"].values
 
 def report(results,classifier, n_top=3):
     file = open(classifier,"w") 
@@ -53,17 +33,12 @@ def report(results,classifier, n_top=3):
 
     file.close() 
 
-#def outputResults(prediction):
-#    df = DataFrame(prediction, columns = ['',''])
-#    results.to_csv("submission.csv")
-
-# In[42]:
 
 
 print("running random forest + finding parameters")
 
 from sklearn.model_selection import GridSearchCV
-#from sklearn.model_selection import RandomizedSearchCV
+
 from sklearn.ensemble import RandomForestClassifier
 
 clf = RandomForestClassifier()
@@ -75,24 +50,7 @@ grid_search = GridSearchCV(clf, param_grid=param_grid,n_jobs=-1,verbose=2, cv=4)
 grid_search.fit(X, y)
 report(grid_search.cv_results_,"randomforestGS.txt")
 
-#random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-    #                               n_iter=500, cv=4,random_state=64,n_jobs=-1,verbose=2)
 
-#random_search.fit(X, y)
-#report(random_search.cv_results_,"randomforest.txt")
-# pip3 install keras tensorflow tensorflow-gpu numpy pandas sklearn
-
-# In[41]:
-
-
-
-#import heapq
-#features = grid_search.best_estimator_.feature_importances_.tolist()
-#print(features)
-
-
-#bestFeaturesIndex = heapq.nlargest(10, xrange(len(features)), key=features.__getitem__)
-#print(bestFeaturesIndex)
 
 
 from sklearn.neighbors import KNeighborsClassifier
@@ -108,10 +66,12 @@ grid_search.fit(X, y)
 report(grid_search.cv_results_,"nearestNGS.txt")
 
 
-#random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-#                                   n_iter=100, cv=4,random_state=64,n_jobs=-1,verbose=2)
-#
-#random_search.fit(X, y)
-#report(random_search.cv_results_,"KNN.txt")
+from sklearn.ensemble import GradientBoostingClassifier
+clf = GradientBoostingClassifier()
 
+param_grid = {"n_estimators": range(200,260,10),
+              "max_depth":  range(10,50,5)}
 
+grid_search = GridSearchCV(clf, param_grid=param_grid,n_jobs=-1,verbose=2, cv=4)
+grid_search.fit(X, y)
+report(grid_search.cv_results_,"boostGS.txt")
